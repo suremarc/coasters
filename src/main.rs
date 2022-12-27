6f43204fa45912604c0ba7c4e088a5ed36782315
suremarc <8771538+suremarc@users.noreply.github.com>
2022-12-26 19:30:04 -0600

    use assets api

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-use bevy::{math::vec3, prelude::*};
-use bevy_flycam::PlayerPlugin;
+#![feature(trivial_bounds)]
+
+use bevy::{math::vec3, prelude::*, reflect::TypeUuid};
+use bevy_flycam::{FlyCam, PlayerPlugin};
 use coasters::proc_mesh::Resampler;
-use pythagorean_hodographs::{Frame, QuinticPHCurve, Spline};
+use iyes_loopless::prelude::*;
+use pythagorean_hodographs::{Curve3, Frame, QuinticPHCurve, Spline};
+use serde::Deserialize;
 
 fn main() {
     App::new()
@@ -16,31 +20,69 @@ fn main() {
         }))
         .add_plugin(PlayerPlugin)
         .insert_resource(Msaa { samples: 4 })
+        .add_asset::<Coaster>()
         .add_startup_system(setup)
-        .add_startup_system(draw_spline)
+        .add_startup_system(init_coasters)
+        .add_system(on_coaster_update)
+        .add_system(bevy::window::close_on_esc)
         .run();
 }
 
-fn draw_spline(
+#[derive(Deref, DerefMut, Component)]
+pub struct BevySpline(Box<dyn Curve3 + Send + Sync + 'static>);
+
+#[derive(Debug, Deserialize, TypeUuid)]
+#[uuid = "db5ca089-b785-4dfc-a407-f9ff009e8715"]
+pub struct Coaster {
+    pts: Vec<Vec3>,
+}
+
+#[derive(Default, Component)]
+struct CoasterJoint {
+    pub pos: f32,
+}
+
+pub fn init_coasters(mut coasters: ResMut<Assets<Coaster>>) {
+    coasters.add(Coaster {
+        pts: vec![
+            vec3(6., 12., 1.),
+            vec3(0., 8., 2.),
+            vec3(3., 4., 7.),
+            vec3(6., 0., 4.),
+            vec3(8., 4., 5.),
+            vec3(12., 2., 6.),
+            vec3(11., 10., 7.),
+            vec3(11., 19., 8.),
+        ],
+    });
+}
+
+pub fn on_coaster_update(
     mut commands: Commands,
+    mut ev_asset: EventReader<AssetEvent<Coaster>>,
+    assets: Res<Assets<Coaster>>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let pts = vec![
-        vec3(6., 12., 1.),
-        vec3(0., 8., 2.),
-        vec3(3., 4., 7.),
-        vec3(6., 0., 4.),
-        vec3(8., 4., 5.),
-        vec3(12., 2., 6.),
-        vec3(11., 10., 7.),
-        vec3(11., 19., 8.),
-    ];
+    for ev in ev_asset.iter() {
+        if let AssetEvent::Created { handle } = ev {
+            let coaster = assets.get(handle).expect("asset should be loaded");
+            draw_coaster(&mut commands, handle, coaster, &mut meshes, &mut materials);
+        }
+    }
+}
 
+fn draw_coaster(
+    commands: &mut Commands,
+    handle: &Handle<Coaster>,
+    coaster: &Coaster,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    materials: &mut ResMut<Assets<StandardMaterial>>,
+) {
     let ds = 0.1;
 
     let start = bevy::utils::Instant::now();
-    let spline = Spline::<QuinticPHCurve>::catmull_rom(&pts);
+    let spline = Spline::<QuinticPHCurve>::catmull_rom(&coaster.pts);
     let duration = start.elapsed();
     println!("{}", duration.as_micros());
 
@@ -49,36 +91,42 @@ fn draw_spline(
     let m_duration = m_start.elapsed();
     println!("{}", m_duration.as_micros());
 
-    for &pt in pts.iter() {
-        commands.spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Icosphere {
-                radius: 0.3,
+    commands
+        .spawn((
+            handle.clone(),
+            BevySpline(Box::new(spline.clone())),
+            PbrBundle {
+                mesh: meshes.add(mesh),
+                material: materials.add(Color::rgb(0.1, 0.4, 0.8).into()),
                 ..Default::default()
-            })),
-            material: materials.add(Color::RED.into()),
-            transform: Transform::from_translation(pt),
-            ..Default::default()
-        });
-    }
+            },
+        ))
+        .with_children(|parent| {
+            for &pt in coaster.pts.iter() {
+                parent.spawn(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Icosphere {
+                        radius: 0.3,
+                        ..Default::default()
+                    })),
+                    material: materials.add(Color::RED.into()),
+                    transform: Transform::from_translation(pt),
+                    ..Default::default()
+                });
+            }
 
-    for frame in Resampler::new(&spline, 0., 1., ds).map(|u| spline.frame(u)) {
-        let position = frame.translation;
-        commands.spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Icosphere {
-                radius: 0.1,
-                ..Default::default()
-            })),
-            material: materials.add(Color::GREEN.into()),
-            transform: Transform::from_translation(Vec3::from(position)),
-            ..Default::default()
+            for frame in Resampler::new(&spline, 0., 1., ds).map(|u| spline.frame(u)) {
+                let position = frame.translation;
+                parent.spawn(PbrBundle {
+                    mesh: meshes.add(Mesh::from(shape::Icosphere {
+                        radius: 0.1,
+                        ..Default::default()
+                    })),
+                    material: materials.add(Color::GREEN.into()),
+                    transform: Transform::from_translation(Vec3::from(position)),
+                    ..Default::default()
+                });
+            }
         });
-    }
-
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(mesh),
-        material: materials.add(Color::rgb(0.1, 0.4, 0.8).into()),
-        ..Default::default()
-    });
 }
 
 fn setup(
